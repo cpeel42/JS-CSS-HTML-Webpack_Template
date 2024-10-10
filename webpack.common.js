1772246347e42ff7f6537a86e18b4795c851c210
cpeel42 <175068649+cpeel42@users.noreply.github.com>
2024-10-09 20:54:56 -0400

    Configure npm merge and split webpack files into dev / prod

--- webpack.common.js
+++ webpack.common.js
@@ -4,17 +4,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+  entry: {
+    app: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   module: {
     rules: [
       {
